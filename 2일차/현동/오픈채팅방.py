b0101c34a18c753a73a14cf2d92fb3308a478e4d
hhhhhhdong <gusehd520@gmail.com>
2022-04-20 23:26:57 +0900

    Update 육현동: 오픈채팅방

--- 2일차/현동/오픈채팅방.py
+++ 2일차/현동/오픈채팅방.py
@@ -4,18 +4,14 @@ def solution(record):
     
     for log in record:
         log = log.split()
-        if log[0] == 'Leave':
-            answer.append(log)
-        elif log[0] == 'Enter':
+        if log[0] == 'Enter' or log[0] == 'Change':
             name[log[1]] = log[2]
-            answer.append(log)
-        elif log[0] == 'Change':
-            name[log[1]] = log[2]
-        
-    for i, log in enumerate(answer):
+    
+    for log in record:
+        log = log.split()
         if log[0] == 'Enter':
-            answer[i] = '{}님이 들어왔습니다.'.format(name[log[1]])
-        else:
-            answer[i] = '{}님이 나갔습니다.'.format(name[log[1]])
+            answer.append('{}님이 들어왔습니다.'.format(name[log[1]]))
+        elif log[0] == 'Leave':
+            answer.append('{}님이 나갔습니다.'.format(name[log[1]]))
     
     return answer
