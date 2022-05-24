3d4dc2b204e5005509e217c6f6e4f2d710b2a11e
kgw012 <kgw012@gmail.com>
2022-05-24 16:46:40 +0900

    Update 김길웅: 행렬 테두리 회전하기

--- 5일차/길웅/행렬테두리회전하기.py
+++ 5일차/길웅/행렬테두리회전하기.py
@@ -1,11 +1,11 @@
 # 프로그래머스 '행렬 테두리 회전하기'
 # https://programmers.co.kr/learn/courses/30/lessons/77485
 
-def rotate(map, query, MAX_NUM):
+def rotate(map, query):
     i1, j1, i2, j2 = query[0] - 1, query[1] - 1, query[2] - 1, query[3] - 1
 
     tmp = map[i1][j1]
-    min_num = MAX_NUM
+    min_num = tmp
 
     for i in range(i1 + 1, i2 + 1):
         map[i - 1][j1] = map[i][j1]
@@ -31,10 +31,9 @@ def rotate(map, query, MAX_NUM):
 
 def solution(rows, columns, queries):
     map = [[j for j in range(columns * i + 1, columns * (i + 1) + 1)] for i in range(rows)]
-    MAX_NUM = len(map) * len(map[0])
 
     answer = []
     for query in queries:
-        answer.append(rotate(map, query, MAX_NUM))
+        answer.append(rotate(map, query))
         
     return answer
