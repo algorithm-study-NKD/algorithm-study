918461984d82696c04c569bd1f479839dc3c4f5a
JosephNaa <js.pekah@gmail.com>
2022-05-31 21:53:27 +0900

    Solve 요셉: 거리두기 확인하기

---
+++ 7일차/요셉/거리두기확인하기.py
@@ -0,0 +1,55 @@
+# 00000
+# 00P12
+# 0X4X0
+# 00500
+
+def check(y, x, place):
+    
+    # p 오른쪽이 p면 false
+    if place[y][x+1] == 'P':
+        return False
+    
+    # p 오른쪽이 o일 때, x축 p+1 하, p+2 확인
+    elif place[y][x+1] == 'O':
+        if place[y][x+2] == 'P' or place[y+1][x+1] == 'P':
+            return False
+    
+    # p 하단이 p면 false
+    if place[y+1][x] == 'P':
+        return False
+    
+    # p 하단이 o일 때, y축 p+1 좌우, p+2 확인
+    elif place[y+1][x] == 'O':
+        if place[y+2][x] == 'P' or place[y+1][x+1] == 'P' or place[y+1][x-1] == 'P':
+            return False
+    
+    # p 왼쪽이 o일 때, y축 p+1 좌 확인
+    if place[y][x-1] == 'O' and place[y+1][x-1] == 'P':
+        return False
+        
+    return True
+        
+
+def find(place):
+    # 범위 안 벗어나도록 패딩 추가
+    place = list(map(lambda p : '0{0}00'.format(p), place))
+    place.extend(['00000000', '00000000'])
+    
+    for i in range(7):
+        for j in range(8):
+            if place[i][j] == 'P':
+                if not check(i, j, place): # 맨해튼 거리 체크
+                    return False
+    return True
+
+def solution(places):
+    answer = []
+    
+    # 순서대로
+    for place in places:
+        if not find(place): # 거리두기 판별
+            answer.append(0)
+        else:
+            answer.append(1)
+        
+    return answer
